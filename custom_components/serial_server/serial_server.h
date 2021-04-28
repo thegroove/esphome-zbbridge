79c0857b3c6ee73f4081620486f63a7b75fee89b
TheGroove <you@example.com>
2021-04-28 05:56:45 +0200

    more housekeeping

--- custom_components/serial_server/serial_server.h
+++ custom_components/serial_server/serial_server.h
@@ -35,13 +35,6 @@
 #include <AsyncTCP.h>
 #endif
 
-#ifdef ARDUINO_ARCH_ESP32
-#include <ESPmDNS.h>
-#endif
-#ifdef ARDUINO_ARCH_ESP8266
-#include <ESP8266mDNS.h>
-#endif
-
 namespace esphome {
 namespace serial_server {
 
